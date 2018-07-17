909fe428a1de3e577299896771280013854cfd8e
Stephanie Metzner <stephaniemetzner@gmail.com>
2018-07-17 00:33:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,9 +15,9 @@ end
 
 
 def take_a_number(katz_deli, name)
-  
-  puts  "Welcome, #{name}. You are number #{katz_deli.size + 1} in line."
-       katz_deli << name
+  katz_deli << name
+  puts  "Welcome, #{name}. You are number #{katz_deli.size} in line."
+       
     
 end
 
